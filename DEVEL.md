b3196f60701f84a3226287695c17fe2c525ea845
Andy Kirkham <andy.kirkham@solarwinds.com>
2018-06-07 11:20:54 +0100

    Add extra docs and instructions

---
+++ DEVEL.md
@@ -0,0 +1,39 @@
+# Developing on K3
+
+## Requirements
+
+A local dev machine running Docker (best use Ubuntu)
+
+## Step 1
+
+Clone the repo.
+
+Build the entire package using the ```build.sh``` script. This ensures all the containers are now built out.
+
+## Step 2
+
+In a seperate bash shell start a k3:dev container and build a local k3 instance:
+
+```
+$ docker run --cap-add=SYS_PTRACE --security-opt=seccomp:unconfined --privileged --rm -v /etc/k3conf.json:/etc/k3conf.json -v `pwd`:/ws -it k3:dev
+bash-4.4# cd /tmp
+bash-4.4# cmake /ws
+bash-4.4# make
+```
+
+You should now have main/k3 program built.
+
+## Step 3
+
+Test your dev build by creating an ```/etc/k3conf.json``` configuration with appropiate values (see README.md)
+
+Run a test
+
+```
+bash-4.4# main/k3
+```
+
+## Step 4
+
+You can now swap back to the other bash shell window and work on the code. To build changes you make swap bash shells and just rerun ```make``` and test again.
+
--- build.sh
+++ build.sh
@@ -8,6 +8,22 @@ AWS_SDK_CPP_VER_PATCH="62"
 
 VER=`head -1 CHANGELOG.txt`
 
+# Start by building a container that holds a static
+# library for SASL. We have to do this as it uses libreSSL
+# rather than OpenSSL and Alpine:3.7 appears to have 
+# a package manager conflict between these two SSL libraries
+# Builds k3:sasl
+#
+# Build the dev builder env that uses COPY --from k3:sasl to bring \
+# in the static SASL library to link against. This dev stage build \
+# librdkafa, libcurl (again Alpine packake manager issues) and the \
+# AWS C++ SDK (all static builds). Finally it builds the k3 app. \
+# Builds k3:dev
+#
+# The last step is to create an image with a COPY --from from the \
+# above to produce a tiny Docker image we can push to the repo. \
+# Builds k3:rel-${VER}
+
 docker build \
 	-f Dockerfile.sasl \
 	--tag k3:sasl \
@@ -23,8 +39,6 @@ docker build \
 	. \
 && docker build -f Dockerfile.rel --tag k3:rel-${VER} . \
 && echo "docker tag k3:rel-${VER} andykirkham/kafka-k3:${VER}" \
-&& echo "docker push andykirkham/kafka-k3:${VER}" \
-&& echo "docker tag k3:rel-${VER} andykirkham/kafka-k3:latest" \
-&& echo "docker push andykirkham/kafka-k3:latest" 
+&& echo "docker push andykirkham/kafka-k3:${VER}" 
 
 
